fefc4efe47747fe84038b2a0e84d810f45fdb7d8
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-17 21:07:42 +0400

    - Fixed secrets referencing

--- components/200-entra/users.tf
+++ components/200-entra/users.tf
@@ -2,5 +2,5 @@ resource "azuread_user" "mervinhemaraju16" {
   user_principal_name = "mervinhemaraju16@gmail.com"
   display_name        = "Mervin Hemaraju"
   mail_nickname       = "mervinhemaraju16"
-  password            = data.doppler_secrets.auth0.map.AZURE_APOLLO_AD_ADMIN_PASSWORD
+  password            = data.doppler_secrets.azure_crds.map.AZURE_APOLLO_AD_ADMIN_PASSWORD
 }
