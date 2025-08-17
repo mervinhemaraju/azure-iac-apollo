763ab2d253f54ae91c6cf03e3fa053340052f1ad
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-17 21:20:04 +0400

    - Added a new user

--- components/200-entra/providers.tf
+++ components/200-entra/providers.tf
@@ -2,6 +2,10 @@
 provider "azuread" {
 }
 
+data "azuread_domains" "default" {
+  only_default = true
+}
+
 # Doppler Provider for Secrets Manager
 provider "doppler" {
   doppler_token = var.token_doppler_global
--- components/200-entra/users.tf
+++ components/200-entra/users.tf
@@ -1,6 +1,8 @@
-resource "azuread_user" "mervinhemaraju16" {
-  user_principal_name = "mervinhemaraju16@gmail.com"
-  display_name        = "Mervin Hemaraju"
-  mail_nickname       = "mervinhemaraju16"
-  password            = data.doppler_secrets.azure_crds.map.AZURE_APOLLO_AD_ADMIN_PASSWORD
+resource "azuread_user" "mervinhemaraju" {
+  user_principal_name   = "mervinhemaraju@${data.azuread_domains.default.domains.0.domain_name}"
+  display_name          = "Mervin Hemaraju"
+  mail_nickname         = "mervinhemaraju"
+  password              = data.doppler_secrets.azure_crds.map.AZURE_APOLLO_AD_ADMIN_PASSWORD
+  account_enabled       = true
+  force_password_change = true
 }
